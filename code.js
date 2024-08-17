13978ce0ec2763ef1163b5b81db9c6889f155f20
Helloworld2518 <ekaadi0906@gmail.com>
2024-08-17 18:03:24 +0700

    update 1.2

--- code.js
+++ code.js
@@ -1,8 +1,24 @@
-const backImage = document.getElementById("next");
-const paket1 = document.getElementById("paket1");
-const paket2 = document.getElementById("paket2");
+count = 0;
+const backImage = document.getElementById("back");
+var paket = document.getElementsByClassName("paket");
+function next() {
+  paket[count].style.display = "none";
+  paket[count + 1].style.display = "table";
+  count++;
+  console.log(count);
+}
 
-backImage.addEventListener("click", () => {
-  paket1.style.display = "none";
-  paket2.style.display = "table";
-});
+function back() {
+  paket[count - 1].style.display = "table";
+  paket[count].style.display = "none";
+  count--;
+}
+
+function send() {
+  var nama = document.getElementById("username").value;
+  var text = document.getElementById("norek").value;
+  var pesan = text;
+  var nomor = "+6281353842621";
+  var value = "https://wa.me/" + nomor + "?text=" + pesan;
+  window.open(value, "_blank").focus();
+}
